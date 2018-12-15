74a3574d4de5e04ccf876036909c5383f68a0efd
Federico Lodovici <f.lodovici@outlook.com>
2018-12-15 17:07:42 +0100

    first attempts

--- Fetchandlear.m
+++ Fetchandlear.m
@@ -1,3 +1,22 @@
 clear all;
-clearvars
-status = system("azcopy --recursive --source https://elabdataset.blob.core.windows.net/thumbnails/ --destination ./images --source-key kJ6skRn9nnZGQUlz2lsrbTHDfkqjV8sm405uG+ou9QwDJBaP37qrScdtxGjGgc3eBs3boK1g2EuAxktRlPR1/A== --preserve-last-modified-time");
+clearvars;
+imds = imageDatastore('images','IncludeSubfolders',true,'LabelSource','foldernames');
+[train,test] = splitEachLabel(imds,0.7,'randomized');
+net = alexnet;
+inputSize = net.Layers(1).InputSize;
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),train);
+augimdsTest = augmentedImageDatastore(inputSize(1:2),test);
+layer = 'fc7';
+featuresTrain = activations(net,augimdsTrain,layer,'OutputAs','rows');
+featuresTest = activations(net,augimdsTest,layer,'OutputAs','rows');
+knn = fitcknn(featuresTrain, train.Labels);
+predicted_test_knn = predict(knn, featuresTest);
+performance_test_knn = confmat(test.Labels, predicted_test_knn);
+figure
+for i = 1:7
+    subplot(2,2,i)
+    I = readimage(test,i);
+    label = predicted_test_knn(i);
+    imshow(I)
+    title(char(label))
+end
