8b07880a5330f1ae17176813ac4f964de10410dc
Federico Lodovici <f.lodovici@outlook.com>
2018-12-15 17:08:20 +0100

    first tries

---
+++ updateimages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+azcopy --recursive --source https://elabdataset.blob.core.windows.net/thumbnails/ --destination ./images --source-key kJ6skRn9nnZGQUlz2lsrbTHDfkqjV8sm405uG+ou9QwDJBaP37qrScdtxGjGgc3eBs3boK1g2EuAxktRlPR1/A== --preserve-last-modified-time
+echo "Updated file list!"
